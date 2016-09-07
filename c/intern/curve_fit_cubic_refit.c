7d2abd48677ce24085dfb33f18e6fe633cc03208
Campbell Barton <ideasman42@gmail.com>
2016-09-07 13:26:10 +1000

    Move assert loop into debug ifdef

--- c/intern/curve_fit_cubic_refit.c
+++ c/intern/curve_fit_cubic_refit.c
@@ -1260,6 +1260,13 @@ int curve_fit_cubic_to_points_refit_db(
 
 #ifdef USE_CORNER_DETECT
 	if (use_corner) {
+
+#ifdef DEBUG
+		for (uint i = 0; i < knots_len; i++) {
+			assert(knots[i].heap_node == NULL);
+		}
+#endif
+
 		knots_len_remaining = curve_incremental_simplify_corners(
 		        &pd, knots, knots_len, knots_len_remaining,
 		        SQUARE(error_threshold), SQUARE(error_threshold * 3),
